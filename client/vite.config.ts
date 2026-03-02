8ea080b7e15e7d5aca97b8ad2bbdb74403a6ee8b
tykimdream <tykimdream@gmail.com>
2026-03-02 22:24:45 +0900

    [config] client 설정 파일 생성 (vite, eslint, tsconfig)

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/__PROJECT_NAME__/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        ws: true,
+      },
+    },
+  },
+});
